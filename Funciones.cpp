1f666c97062231effc1b68adcf4ddaa8c4f453fc
Joaquin Arroniz Rosas <79723164+Joaquin2AV6@users.noreply.github.com>
2021-03-03 16:33:09 -0600

    Agregando dos tareas a mi repositorio.

---
+++ Funciones.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int var1;
+    float var2;
+    char var3;
+
+    cout<<"PRESENTANDO MI PRIMER PROGRAMA EN C++"<<endl<<endl;
+
+        cout<<"Digite un numero : ";
+        cin>>var1;
+        cout<<"El numero es : "<<var1<<endl<<endl;
+
+        cout<<"Digite un flotante : ";
+        cin>>var2;
+        cout<<"El numero es : "<<var2<<endl<<endl;
+
+        cout<<"Introduzca una letra : ";
+        cin>>var3;
+        cout<<"La letra es : "<<var3<<endl<<endl;
+
+
+    return 0;
+
+}
---
+++ HOLAMUND_ARRONIZ_ROSAS_JOAQUIN.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+      cout<<"HOLA JOAQUIN ARRONIZ ROSAS"<<endl<<endl;
+      cout<<"BIENVENIDO AL CURSO DE PROGRAMACION ORIENTADA A OBJETOS"<<endl;
+      cout<<"INSTITUTO POLITECNICO NACIONAL"<<endl<<endl;
+      cout<<"2AV6"<<endl<<endl<<endl<<endl;
+
+
+      getch();
+      return 0;
+}
